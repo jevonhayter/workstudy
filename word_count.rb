5b61cb4e7e54adde897cf6398d6d2c29b85a8f7f
Jevon Hayter <jevonhayter@gmail.com>
2017-01-08 07:09:02 -0500

    Word Count Program

--- word_count.rb
+++ word_count.rb
@@ -5,21 +5,22 @@ class Phrase
   end
 
   def phrase
+    # Convert the string to clean array of words
     @phrase.tr(',', ' ')
            .delete(':')
            .split
            .each do |word|
-      word.gsub!(/[^abcdefghijklmnopqrstuvwxyz0123456789]/, '')
+      word.gsub!(/[^abcdefghijklmnopqrstuvwxyz0123456789']/, '')
+      word.gsub!(/^\'|\'?$/, '')
     end
   end
 
-  def count_words
-    @word_keeper = {}
+  def word_count
+    word_keeper = {}
 
     phrase.each do |word|
-      @word_keeper[word] = phrase.count(word)
+      word_keeper[word] = phrase.count(word)
     end
-    @word_keeper
+    word_keeper
   end
 end
-
