81569dbf5070989325d45168eea3060e5a68579e
Jevon Hayter <jevonhayter@gmail.com>
2017-01-11 17:02:25 -0500

    Bowling Program

--- bowling.rb
+++ bowling.rb
@@ -1,145 +1,114 @@
+# Program to score a bowling game
 class Game
   attr_reader :rolls
- 
+
   def initialize
-    @rolls = Array.new
+    @rolls = []
   end
-  #Exceptions --------------------------------------------------
- 
-  def pin_value_exception(pin)
-    raise RuntimeError, 'Pins must have a value from 0 to 10' unless (0..10).include?(pin)
-  end
- 
-  def lane_pin_count(rolls)
-    if !rolls.include?(10) && rolls.take(2).reduce(:+) > 10
-      raise RuntimeError, 'Pin count exceeds pins on the lane'
-    elsif rolls[18] == 10 && !rolls[19..20].include?(10) && rolls[19..20].reduce(:+).to_i  > 10
-      raise RuntimeError, 'Pin count exceeds pins on the lane'
-    end
-  end
- 
-  def end_of_game?(rolls)
-    if !rolls.include?(10) && rolls.size < 20
-      raise RuntimeError, 'Score cannot be taken until the end of the game'
-    end
-  end
- 
-  def after_game?(rolls)
-    if !rolls.include?(10) && rolls.size > 20 && rolls.slice(18..19).reduce(:+) != 10
-      raise RuntimeError, 'Should not be able to roll after game is over' 
-    end
-  end
- 
-  def perfect_score_end_of_game?(rolls)
-    if rolls.size < 12 && rolls.all? { |pins| pins == 10 }
-      raise RuntimeError, 'Game is not yet over, cannot score!' #if @rolls.size == 10
-    end
-  end
- 
-  
-  
+
   def roll(pins)
-    pin_value_exception(pins)
-   
+    check_pins_are_valid?(pins)
+
     @rolls << pins
-   
-    lane_pin_count(rolls)
-    after_game?(rolls)
+
+    check_frame_pins_count_valid?(rolls)
+    check_if_game_is_over?(rolls)
   end
- 
+
   def score
-    end_of_game?(rolls)
-    perfect_score_end_of_game?(rolls)
-   
+    check_we_have_reached_end_of_game?(rolls)
+    check_perfect_score_end_of_game?(rolls)
+
     return 300 if rolls.all? { |pins| pins == 10 }
-   
+
+    # Calculates all strikes in the game
     if strikes_that_are_not_last_frame?(rolls)
       strike_calculation(rolls)
+
+    # Calculates all spares in the game
     elsif spares_that_are_not_last_frame?(rolls)
       spare_calculation(rolls)
-    elsif last_frame_strike?(rolls)
-      (rolls.fetch(18) + rolls.fetch(19) + rolls.fetch(20)) + rolls.slice(0, 18).reduce(:+)
-    elsif last_frame_spare?(rolls)
-      spare_calculation(rolls)
-    elsif last_frame_open?(rolls)
-      (rolls.fetch(18) + rolls.fetch(19) + rolls.fetch(20)) + rolls.slice(0, 18).reduce(:+)
-    elsif last_frame_all_stikes?(rolls)
-      spare_calculation(rolls)
+
+    # Calculates all open frames
     else
-     rolls.reduce(:+)
+      rolls.reduce(:+)
     end
   end
- 
-  #Helper Methods
- 
+
+  # Helper Methods
   def strikes_that_are_not_last_frame?(rolls)
-    return rolls if rolls.slice(0, 17).include?(10) && !rolls.slice(17..20).include?(10)
+    rolls if rolls.slice(0, 17).include?(10) && !rolls.slice(17..20).include?(10)
   end
- 
+
   def spares_that_are_not_last_frame?(rolls)
-    return rolls if rolls.take(2).reduce(:+) == 10 && !rolls.slice(18..-1).include?(10)
+    rolls if rolls.take(2).reduce(:+) == 10 && !rolls.slice(18..-1).include?(10)
   end
- 
-  def last_frame_strike?(rolls)
-    rolls.fetch(18) == 10
+
+  # Exceptions
+  def check_pins_are_valid?(pin)
+    raise 'Pins must have a value from 0 to 10' unless (0..10).cover?(pin)
   end
- 
-  def last_frame_open?(rolls)
-    rolls.fetch(18) == 10 && rolls.slice(19..20)
-  end
- 
-  def last_frame_spare?(rolls)
-    rolls.slice(18..19) == 10
-  end
- 
-  def last_frame_all_stikes?(rolls)
-    rolls.slice(18..10) == 30
-  end
- 
-  
-  
-  def roll_n_times(rolls, pins)
-    rolls.times do
-      Array(pins).each { |value| roll(value) }
+
+  def check_frame_pins_count_valid?(rolls)
+    if !rolls.include?(10) && rolls.take(2).reduce(:+) > 10
+      raise 'Pin count exceeds pins on the lane'
+    elsif rolls[18] == 10 && !rolls[19..20].include?(10) && rolls[19..20].reduce(:+).to_i > 10
+      raise 'Pin count exceeds pins on the lane'
     end
   end
- 
-  
+
+  def check_we_have_reached_end_of_game?(rolls)
+    raise 'Score cannot be taken until the end of the game' if
+    !rolls.include?(10) && rolls.size < 20
+  end
+
+  def check_if_game_is_over?(rolls)
+    raise 'Should not be able to roll after game is over' if
+    !rolls.include?(10) && rolls.size > 20 && rolls.slice(18..19).reduce(:+) != 10
+  end
+
+  def check_perfect_score_end_of_game?(rolls)
+    raise 'Game is not yet over, cannot score!' if
+    rolls.size < 12 && rolls.all? { |pins| pins == 10 }
+  end
+
+  private
+
   def strike_calculation(rolls)
-     find_index_of_tens = rolls.each_index
-                               .select { |index| rolls[index] == 10 }
-     strike_numbers = find_index_of_tens.map { |i| rolls.slice(i, 3) }
-    strikes_reduced = strike_numbers.map { |i| i.reduce(:+) }
-    strike_value = strikes_reduced.reduce(:+)
-   
+    find_index_of_strikes =
+      rolls.each_index.select { |index| rolls[index] == 10 }
+
+    strike_numbers = find_index_of_strikes.map { |i| rolls.slice(i, 3) }
+                                          .map { |i| i.reduce(:+) }
+                                          .reduce(:+)
+
     rolls_wt_strikes = rolls.each_index
                             .select { |index| rolls[index] != 10 }
-     without_strike_numbers = rolls_wt_strikes.map { |i| rolls[i] }
-     without_stike_value = without_strike_numbers.reduce(:+)
-     strike_value + without_stike_value
- end
- 
-def spare_calculation(rolls)
-    value_10_pairs = rolls.each_slice(2)
-                           .select { |slice| slice.reduce(:+) == 10 }
-     pair_indexes = value_10_pairs.map { |pair| rolls.rindex(pair[1]) }
-     find_pin_index = pair_indexes.map { |index| index + 1 }
- 
- 
-     counter = 0
-     spare_arrays = value_10_pairs.each do |pair|
-                      pair << rolls[find_pin_index[counter]]
-                      counter += 1
-                    end
- 
- 
-     arrays_spare_value = spare_arrays.map { |pair| pair.reduce(:+) }
-     spare_total = arrays_spare_value.reduce(:+)
-     not_value_10_pairs = rolls.each_slice(2)
+
+    without_strike_numbers = rolls_wt_strikes.map { |i| rolls[i] }.reduce(:+)
+    strike_numbers + without_strike_numbers
+  end
+
+  def spare_calculation(rolls)
+    value_10_pairs =
+      rolls.each_slice(2).select { |slice| slice.reduce(:+) == 10 }
+
+    pair_indexes = value_10_pairs.map { |pair| rolls.rindex(pair[1]) }
+                                 .map { |index| index + 1 }
+
+    counter = 0
+    spare_arrays =
+      value_10_pairs.each do |pair|
+        pair << rolls[pair_indexes[counter]]
+        counter += 1
+      end
+
+    arrays_spare_value = spare_arrays.map { |pair| pair.reduce(:+) }.reduce(:+)
+
+    not_value_10_pairs = rolls.each_slice(2)
                               .select { |slice| slice.reduce(:+) != 10 }
-     single_pins = not_value_10_pairs.flatten.reduce(:+)
-     spare_total + single_pins
- end
+                              .flatten.reduce(:+)
+
+    arrays_spare_value + not_value_10_pairs
+  end
 end
- 
- 
--- game_test.rb
+++ game_test.rb
@@ -16,7 +16,6 @@ class GameTest < Minitest::Test
   end
 
   def test_should_be_able_to_score_multiple_frames
-    
     [3, 4, 2, 3, 5, 2].each do |pins|
       @game.roll pins
     end
@@ -25,25 +24,21 @@ class GameTest < Minitest::Test
   end
 
   def test_should_score_a_game_with_all_gutterballs
-    
     roll_n_times(20, 0)
     assert_equal 0, @game.score
   end
 
-    def test_should_score_a_game_with_all_single_pin_rolls
-      
+  def test_should_score_a_game_with_all_single_pin_rolls 
     roll_n_times(20, 1)
     assert_equal 20, @game.score
   end
 
   def test_should_allow_game_with_all_open_frames
-    
     roll_n_times(10, [3, 6])
     assert_equal 90, @game.score
   end
 
   def test_should_correctly_score_a_strike_that_is_not_on_the_last_frame
-    
     @game.roll(10)
     @game.roll(5)
     @game.roll(3)
@@ -53,7 +48,6 @@ class GameTest < Minitest::Test
   end
 
   def test_should_score_a_spare_that_is_not_on_the_last_frame
-    
     @game.roll(5)
     @game.roll(5)
     @game.roll(3)
@@ -64,7 +58,6 @@ class GameTest < Minitest::Test
   end
 
   def test_should_score_multiple_strikes_in_a_row
-    
     @game.roll(10)
     @game.roll(10)
     @game.roll(10)
@@ -75,8 +68,7 @@ class GameTest < Minitest::Test
     assert_equal 81, @game.score
   end
 
-    def test_should_score_multiple_spares_in_a_row
-      
+  def test_should_score_multiple_spares_in_a_row 
     @game.roll(5)
     @game.roll(5)
     @game.roll(3)
@@ -89,7 +81,6 @@ class GameTest < Minitest::Test
   end
 
   def test_should_allow_fill_balls_when_the_final_frame_is_strike
-    
     roll_n_times(18, 0)
     @game.roll(10)
     @game.roll(7)
@@ -99,7 +90,6 @@ class GameTest < Minitest::Test
   end
 
   def test_should_allow_fill_ball_in_last_frame_if_spare
-    
     roll_n_times(18, 0)
     @game.roll(9)
     @game.roll(1)
@@ -109,7 +99,6 @@ class GameTest < Minitest::Test
   end
 
   def test_should_allow_fill_balls_to_be_strike
-    
     roll_n_times(18, 0)
     @game.roll(10)
     @game.roll(10)
@@ -124,7 +113,6 @@ class GameTest < Minitest::Test
   end
 
   def test_should_not_allow_rolls_with_negative_pins
-    
     assert_raises(
       RuntimeError,
       'Pins must have a value from 0 to 10') do
@@ -133,7 +121,6 @@ class GameTest < Minitest::Test
   end
 
   def test_should_not_allow_rolls_better_than_strike
-    
     assert_raises(
       RuntimeError,
       'Pins must have a value from 0 to 10') do
@@ -142,7 +129,6 @@ class GameTest < Minitest::Test
   end
 
   def test_should_not_allow_two_normal_rolls_better_than_strike
-    
     assert_raises RuntimeError, 'Pin count exceeds pins on the lane' do
       @game.roll(5)
       @game.roll(6)
@@ -150,7 +136,6 @@ class GameTest < Minitest::Test
   end
 
   def test_should_not_allow_two_normal_rolls_better_than_strike_in_last_frame
-    
     roll_n_times(18, 0)
     assert_raises RuntimeError, 'Pin count exceeds pins on the lane' do
       @game.roll(10)
@@ -160,7 +145,6 @@ class GameTest < Minitest::Test
   end
 
   def test_should_not_allow_to_take_score_at_the_beginning
-    
     assert_raises(
       RuntimeError,
       'Score cannot be taken until the end of the game',
@@ -170,7 +154,6 @@ class GameTest < Minitest::Test
   end
 
   def test_should_not_allow_to_take_score_before_game_has_ended
-     
     roll_n_times(19, 5)
     assert_raises(
       RuntimeError,
@@ -180,7 +163,6 @@ class GameTest < Minitest::Test
   end
 
   def test_should_not_allow_rolls_after_the_tenth_frame
-    
     roll_n_times(20, 0)
     assert_raises(
       RuntimeError,
@@ -191,7 +173,6 @@ class GameTest < Minitest::Test
   end
 
   def test_should_not_calculate_score_before_fill_balls_have_been_played
-   
     roll_n_times(10, 10)
 
     assert_raises RuntimeError, 'Game is not yet over, cannot score!' do
